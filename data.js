7f00edc433dc65e6c2f04aae9e79ad83bd51d577
Hej-eon <heja.abdalla@whitecityplace.com>
2026-07-10 11:30:47 +0100

    Update data.js

--- data.js
+++ data.js
@@ -68,7 +68,7 @@ const eliminatedTeams = [
   "Ivory Coast","Sweden","Panama","Czech Republic","Netherlands","South Africa",
   "Ecuador","Uzbekistan","Turkey","Curacao","Uruguay","DR Congo","Scotland",
   "Germany","Japan","Qatar","Saudi Arabia","Haiti","Austria","Croatia","Algeria",
-  "Australia","Cape Verde","Ghana","Canada","Paraguay","Brazil", "Mexico", "Portugal", "USA", "Egypt", "Colombia"
+  "Australia","Cape Verde","Ghana","Canada","Paraguay","Brazil", "Mexico", "Portugal", "USA", "Egypt", "Colombia", "Morocca"
 ];
 
 const fixtures = [
@@ -100,7 +100,7 @@ const fixtures = [
 
   { id:"QF-HEADER", date:"QUARTER FINALS", home:{name:"",code:"",owner:""}, away:{name:"",code:"",owner:""}, result:"", winner:"" },
 
-  { id:"qf-1", date:"09 July 2026 - 9:00 PM", home:{name:"France",code:"fr",owner:"Karl"}, away:{name:"Morocco",code:"ma",owner:"Darshan"}, result:"", winner:"" },
+  { id:"qf-1", date:"09 July 2026 - 9:00 PM", home:{name:"France",code:"fr",owner:"Karl"}, away:{name:"Morocco",code:"ma",owner:"Darshan"}, result:"2-0", winner:"France" },
   { id:"qf-2", date:"10 July 2026 - 8:00 PM", home:{name:"Spain",code:"es",owner:"Betim"}, away:{name:"Belgium",code:"be",owner:"Darshan"}, result:"", winner:"" },
   { id:"qf-3", date:"11 July 2026 - 10:00 PM", home:{name:"Norway",code:"no",owner:"Darshan"}, away:{name:"England",code:"eng",owner:"Krishan"}, result:"", winner:"" },
   { id:"qf-4", date:"12 July 2026 - 2:00 AM", home:{name:"Argentina",code:"ar",owner:"Darshan"}, away:{name:"Switzerland",code:"ch",owner:"Krishan"}, result:"", winner:"" }
